f5a7841ea5c5c8f55026c6405effe78b82a25893
Willian Antunes <willianBarone9@gmail.com>
2024-04-06 19:00:33 -0300

    Number and date formatting

--- SalesMVC/Program.cs
+++ SalesMVC/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesMVC.Data;
 using SalesMVC.Models;
 using SalesMVC.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,10 +20,19 @@ builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
 
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+
+
 var app = builder.Build();
 
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
-
+app.UseRequestLocalization(localizationOptions);
 
 if (!app.Environment.IsDevelopment())
 {
